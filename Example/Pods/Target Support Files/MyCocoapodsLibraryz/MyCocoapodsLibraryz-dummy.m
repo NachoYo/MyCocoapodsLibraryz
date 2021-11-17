c075b936f5dc8ec94fabbaf4f81754c5dc3de05a
Luis Ignacio Viñas Petriz <luis.ignacio.vinas.petriz@everis.com>
2021-11-17 09:17:32 -0600

    fasdf

---
+++ Example/Pods/Target Support Files/MyCocoapodsLibraryz/MyCocoapodsLibraryz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyCocoapodsLibraryz : NSObject
+@end
+@implementation PodsDummy_MyCocoapodsLibraryz
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyCocoapodsLibraryz_Example/Pods-MyCocoapodsLibraryz_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyCocoapodsLibraryz_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyCocoapodsLibraryz_Example
+@end
